4e3491829ae3eb9a978e7822d563185516f0bec8
Thiago <e107ti@cps.sp.gov.br>
2022-02-25 21:52:19 -0300

    terminei as licao

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Santos!");
+Console.WriteLine("São Vicente!");
+Console.WriteLine("Praia Grande!");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Peruíb");
+Console.WriteLine("Ilha Comprida.");
+Console.WriteLine("Iguape");
